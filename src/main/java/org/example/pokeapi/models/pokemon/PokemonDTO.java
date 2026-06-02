c60f0a25872291320847cee800efd8b6056e3515
Ilham Muhammad <ilhamuh97@gmail.com>
2026-06-02 14:18:17 +0200

    fix validation message

--- src/main/java/org/example/pokeapi/models/pokemon/PokemonDTO.java
+++ src/main/java/org/example/pokeapi/models/pokemon/PokemonDTO.java
@@ -4,9 +4,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record PokemonDTO(
-        @NotBlank(message="Pokemon name must be blank")
+        @NotBlank(message="Pokemon name must not be blank")
         String pokemonName,
 
+        @NotBlank(message="Pokemon name must not be blank")
         @Size(min=5, max=15, message="Nick name must be between 5 and 15 characters")
         String nickname) {
 }
